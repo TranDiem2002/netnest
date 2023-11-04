7d938bd9ceb509d3ad3482a22008cd218e36f9ca
Tran Diem <diemtrantran0102@gmail.com>
2023-11-04 23:42:26 +0700

    add project

---
+++ webchat/src/test/java/com/netnest/webchat/WebchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netnest.webchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
